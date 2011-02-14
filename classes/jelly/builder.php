bc89cee77dc33b474fc7d6ee5942825aa2960527
Matt Oakley <matt@creativeintent.co.uk>
2011-02-14 17:18:53 +0000

    Fix Jelly_Builder::count() to ignore rows marked as deleted.

--- classes/jelly/builder.php
+++ classes/jelly/builder.php
@@ -12,4 +12,14 @@ class Jelly_Builder extends Jelly_Builder_Core
 		return parent::execute($db);
 	}
 	
+	public function count($db = 'default')
+	{
+		if (is_object($this->_meta) AND $this->_meta->columns('deleted'))
+		{
+			$this->where('deleted', 'IS', NULL);	
+		}
+		
+		return parent::count($db);
+	}
+	
 }
