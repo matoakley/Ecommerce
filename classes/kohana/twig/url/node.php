7a4ae99e8ad916679ec108211239c3bccabc7118
Matt Oakley <matt@creativeintent.co.uk>
2011-12-13 13:09:08 +0000

    Overload Twig url function to persist current HTTP protocol

---
+++ classes/kohana/twig/url/node.php
@@ -0,0 +1,53 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+/**
+ * Parses a {% url %} tag
+ *
+ * @package kohana-twig
+ * @author Jonathan Geiger
+ */
+class Kohana_Twig_Url_Node extends Twig_Node
+{
+	/**
+	 * The route to invoke
+	 * @var object
+	 */
+	protected $route;
+	
+	/**
+	 * Parameters to pass to the route
+	 * @var object
+	 */
+	protected $params;
+
+	/**
+	 * Compiles the tag
+	 *
+	 * @param object $compiler 
+	 * @return void
+	 * @author Jonathan Geiger
+	 */
+	public function compile($compiler)
+	{
+		if ($this->params)
+		{
+			$compiler
+				->write('$route_params = ')
+				->subcompile($this->params)
+				->raw(";\n");
+		}
+		else
+		{
+			$compiler
+				->write('$route_params = array()')
+				->raw(";\n");
+		}
+
+		// Output the route
+		$compiler
+			->write('echo Route::url(')
+			->subcompile($this->route)
+			->write(', $route_params, '.Request::$protocol.')')
+			->raw(";\n");
+	}
+}
