83bd079348c20ecc139d903ea38eb89c4dea809e
Matt Oakley <matt@creativeintent.co.uk>
2012-01-03 13:17:23 +0000

    Fixing routes for sitemap

--- classes/ecommerce/controller/pages.php
+++ classes/ecommerce/controller/pages.php
@@ -11,7 +11,7 @@ class Ecommerce_Controller_Pages extends Controller_Application {
 		
 		if($this->request->action == 'static')
 		{
-			$this->template = 'pages/static/'.$this->request->param('id');
+			$this->template = 'pages/static/'.$this->request->param('slug', $this->request->param('id', FALSE));
 		}
 		
 		parent::before();
@@ -25,14 +25,13 @@ class Ecommerce_Controller_Pages extends Controller_Application {
 		{
 			throw new Kohana_Exception('Page not found');
 		}
-		
 		$this->template->page = $page;
 		
 		$this->add_breadcrumb('/pages/view/' . $page->slug, $page->name);
 	}
 	
 	function action_static($slug = FALSE)
-	{
+	{	
 		if ($slug = 'home')
 		{
 			$this->template->featured_products = Jelly::select('product')
