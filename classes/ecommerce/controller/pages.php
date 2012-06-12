ec46e1bc1f0620982fc3972ff60d40eb8815bab7
Matt Oakley <matt@creativeintent.co.uk>
2012-06-12 10:25:39 +0100

    Massively improved updgrade to static page slugs.

--- classes/ecommerce/controller/pages.php
+++ classes/ecommerce/controller/pages.php
@@ -35,9 +35,20 @@ class Ecommerce_Controller_Pages extends Controller_Application {
 		$this->add_breadcrumb(URL::site(Route::get('view_page')->uri(array('slug' => $page->slug))), $page->name);
 	}
 	
-	function action_static($slug = FALSE)
+	function action_static()
 	{	
-		if ($slug = 'home')
+		$slug = $this->request->param('slug');
+
+		if (is_array($slug))
+		{
+			$slug_parts = $slug;	
+		}
+		else
+		{
+			$slug_parts[] = $slug;
+		}
+
+		if (end($slug_parts) == 'home')
 		{
 			$this->template->featured_products = Jelly::select('product')
 														->where('status', '=', 'active')
@@ -46,6 +57,30 @@ class Ecommerce_Controller_Pages extends Controller_Application {
 														->limit(4)								
 														->execute();
 		}
+		
+		// build breadcrumb
+		// Try to find a route based on the static page name otherwise use the standard /pages/static/<slug> route
+		try
+		{
+			foreach ($slug_parts as $slug_part)
+			{
+				try
+				{
+					$page_name = ucwords(Inflector::humanize($slug_part));
+					$this->add_breadcrumb(URL::site(Route::get($slug_part)->uri()), $page_name);
+				}
+				catch (Kohana_Exception $e)
+				{
+					continue;
+				}
+			}
+		}
+		catch (Kohana_Exception $e)
+		{
+			$this->add_breadcrumb(URL::site(Route::get('view_static_page')->uri(array('slug' => $slug))), $page_name);			
+		}
+		
+		$this->template = Twig::factory('pages/static/'.end($slug_parts));
 	}
 	
 }
