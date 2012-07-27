e1c6f6430fbc11759b742837580bd71a416e5f0f
Kyle Baggott <kyle@creativeintent.co.uk>
2012-07-27 14:55:59 +0100

    Finally fixed the annoying ckeditor p tag issue

--- media/js/admin/ckeditor/config.js
+++ media/js/admin/ckeditor/config.js
@@ -9,19 +9,20 @@ CKEDITOR.editorConfig = function( config )
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
 	config.removePlugins = 'scayt';
-	
+	config.enterMode = CKEDITOR.ENTER_BR;
+	config.autoParagraph = false;
 	CKEDITOR.on('instanceReady', function( ev ) {
 	  var blockTags = ['div','h1','h2','h3','h4','h5','h6','p','pre','li','blockquote','ul','ol',
-	  'table','thead','tbody','tfoot','td','th',];
+	  'table','thead','tbody','tfoot','td','th','p'];
 	
 	  for (var i = 0; i < blockTags.length; i++)
 	  {
 	     ev.editor.dataProcessor.writer.setRules( blockTags[i], {
 	        indent : false,
-	        breakBeforeOpen : true,
+	        breakBeforeOpen : false,
 	        breakAfterOpen : false,
 	        breakBeforeClose : false,
-	        breakAfterClose : true
+	        breakAfterClose : false
 	     });
 	  }
   });
