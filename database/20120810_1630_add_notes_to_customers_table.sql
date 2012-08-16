6393d1aa165170e610a15ce7ea677c0f52783e85
Matt Oakley <matt@creativeintent.co.uk>
2012-08-16 10:00:26 +0100

    Fix comment and SQL

--- database/20120810_1630_add_notes_to_customers_table.sql
+++ database/20120810_1630_add_notes_to_customers_table.sql
@@ -1,2 +1,2 @@
-/* Creating notes field in customers table */
-ALTER TABLE `paddy_and_scotts`.`customers` ADD COLUMN `notes` longtext;
+-- Creating notes field in customers table
+ALTER TABLE `customers` ADD COLUMN `notes` test;
