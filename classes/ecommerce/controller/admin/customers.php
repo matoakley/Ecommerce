8923a15ea27ddd6ccd0073384cac30b4a5e37a1a
Kyle Baggott <kyle@creativeintent.co.uk>
2012-09-13 11:26:05 +0100

    added show trade only to admin customers

--- classes/ecommerce/controller/admin/customers.php
+++ classes/ecommerce/controller/admin/customers.php
@@ -15,9 +15,9 @@ class Ecommerce_Controller_Admin_Customers extends Controller_Admin_Application
 	public function action_index()
 	{
 		$items = ($this->list_option != 'all') ? $this->list_option : FALSE;
-
+		
 		$search = Model_Customer::search(array(), $items, FALSE, isset($_GET['include_archived']));
-
+			
 		// Pagination
 		$this->template->pagination = Pagination::factory(array(
 			'total_items'  => $search['count_all'],
@@ -33,10 +33,9 @@ class Ecommerce_Controller_Admin_Customers extends Controller_Admin_Application
 		$this->template->total_customers = $search['count_all'];
 		$this->template->page = (isset($_GET['page'])) ? $_GET['page'] : 1;
 		$this->template->items = $items;
-		
 		$this->template->showing_archived = isset($_GET['include_archived']);
 	}
-	
+		
 	public function action_edit()
 	{
 		$customer = Model_Customer::load($this->request->param('id'));
