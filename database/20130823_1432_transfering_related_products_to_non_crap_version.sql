6264bb2b516a9ec589e383bd27ff5a23625a5872
Kyle Baggott <Kyle_baggott@hotmail.co.uk>
2013-08-23 14:46:45 +0100

    amending the sql

--- database/20130823_1432_transfering_related_products_to_non_crap_version.sql
+++ database/20130823_1432_transfering_related_products_to_non_crap_version.sql
@@ -1,1 +1,13 @@
-ALTER TABLE `blog_posts` ADD COLUMN `counter` int(11) DEFAULT '0';
+CREATE TABLE `related_products_products` ( `related_product_id` int, `product_id` int );
+
+insert into related_products_products
+select related_id, product_id
+from related_products
+join products
+on products.id = product_id
+where products.deleted IS NULL
+and related_products.deleted IS NULL
+and product_id IS NOT NULL
+and related_id IS NOT NULL;
+
+-- drop table related_products
