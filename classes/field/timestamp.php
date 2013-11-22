9fce6de563e04e569ba1bba12a7a6291f7a533f5
Kyle Baggott <Kyle_baggott@hotmail.co.uk>
2013-11-22 09:21:59 +0000

    adding a fix for Jelly (it no longer tries to validate a date by the string date rather than timestamp first)

---
+++ classes/field/timestamp.php
@@ -0,0 +1,21 @@
+Class Field_Timestamp extends Jelly_Field_Timestamp
+{
+ 	public function set($value)
+	{
+		if ($value === NULL OR ($this->null AND empty($value)))
+		{
+			return NULL;
+		}
+
+		if (is_numeric($value))
+		{
+			return (int) $value;
+		}
+		elseif (FALSE !== strtotime($value))
+		{
+			return strtotime($value);
+		}
+
+		return $value;
+	}
+}
