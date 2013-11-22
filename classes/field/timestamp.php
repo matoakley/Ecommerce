79e2f7ca44c302f790061794bcb36cb4cf76f1a3
Kyle Baggott <Kyle_baggott@hotmail.co.uk>
2013-11-22 12:38:31 +0000

    fixing bug in jelly timestamp field

--- classes/field/timestamp.php
+++ classes/field/timestamp.php
@@ -1,4 +1,6 @@
-Class Field_Timestamp extends Jelly_Field_Timestamp
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Field_Timestamp extends Jelly_Field_Timestamp
 {
  	public function set($value)
 	{
